37cdc162df34b33898a3583e79f3683fea8cf126
Pedro Delgadillo <Pedelgadillo@gmail.com>
2023-12-01 13:47:34 -0600

    init spring project

---
+++ src/test/java/com/dillo/hireme/HireMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dillo.hireme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
